d989029362d0c95abdad1655dca551eff02f5648
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 21:49:25 -0700

    question 1

---
+++ Chapter 35 to 38/question # 1/add.js
@@ -0,0 +1,9 @@
+// Write a function that displays current date & time in your
+// browser
+function tellTime() {
+     var now = new Date();
+    document.write(now)
+     }
+    tellTime();
+   
+   
