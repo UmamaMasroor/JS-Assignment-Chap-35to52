62a1c623c2600ff24e5f90897bf990c61b7aa24d
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 22:12:24 -0700

    question 3

---
+++ Chapter 35 to 38/question # 3/add.js
@@ -0,0 +1,5 @@
+// Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+const addition = () => parseInt(prompt("Enter the first number: ")) + parseInt(prompt("Enter the second number: "));
+document.write(addition());
