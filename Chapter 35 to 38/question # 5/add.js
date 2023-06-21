b4c5917708d9156f32da70f56ccdb1a8697414a3
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 22:56:00 -0700

    question 5

---
+++ Chapter 35 to 38/question # 5/add.js
@@ -0,0 +1,7 @@
+// Write a function that squares its argument.
+function square(number){
+    return number * number;
+}
+const num = prompt("Enter number");
+const result = square (num);
+document.write(result);
